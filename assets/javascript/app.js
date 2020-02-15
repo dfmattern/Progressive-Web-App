ddec1b16889b9f2a4a2cbd0b4c86fefbc3bafc60
Dane <56905214+dfmattern@users.noreply.github.com>
2020-02-15 00:37:22 -0700

    created html framework, styled with css, created coffee object in js

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+const container = document.querySelector(".container")
+const coffees =[
+    {name:"Espresso", image:"assets/images/espresso.jpg"},
+    {name:"Americano", image:"assets/images/americano.jpg"},
+    {name:"Cappuccino", image:"assets/images/capuccino.jpg"},
+    {name:"Latte", image:"assets/images/latte.jpg"},
+    {name:"Mocha", image:"assets/images/mocha.jpg"},
+    {name:"Machiato", image:"assets/images/machiato.jpg"},
+    {name:"Breve", image:"assets/images/breve.jpg"},
+    {name:"Cafe au Lait", image:"assets/images/cafeaulait.jpg"},
+    {name:"Ristretto", image:"assets/images/ristretto.jpg"},
+]
