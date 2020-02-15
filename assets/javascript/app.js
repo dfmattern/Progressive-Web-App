836cc8653ddc1590b44a0411a5c6d55909129593
Dane <56905214+dfmattern@users.noreply.github.com>
2020-02-15 00:53:06 -0700

    renamed manifest file to manifest.json

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,33 @@
-const container = document.querySelector(".container")
-const coffees =[
-    {name:"Espresso", image:"assets/images/espresso.jpg"},
-    {name:"Americano", image:"assets/images/americano.jpg"},
-    {name:"Cappuccino", image:"assets/images/capuccino.jpg"},
-    {name:"Latte", image:"assets/images/latte.jpg"},
-    {name:"Mocha", image:"assets/images/mocha.jpg"},
-    {name:"Machiato", image:"assets/images/machiato.jpg"},
-    {name:"Breve", image:"assets/images/breve.jpg"},
-    {name:"Cafe au Lait", image:"assets/images/cafeaulait.jpg"},
-    {name:"Ristretto", image:"assets/images/ristretto.jpg"},
-]
+const container = document.querySelector(".container");
+const coffees = [
+  { name: "Espresso", image: "assets/images/espresso.jpg" },
+  { name: "Americano", image: "assets/images/americano.jpg" },
+  { name: "Cappuccino", image: "assets/images/capuccino.jpg" },
+  { name: "Latte", image: "assets/images/latte.jpg" },
+  { name: "Mocha", image: "assets/images/mocha.jpg" },
+  { name: "Machiato", image: "assets/images/machiato.jpg" },
+  { name: "Breve", image: "assets/images/breve.jpg" },
+  { name: "Cafe au Lait", image: "assets/images/cafeaulait.jpg" },
+  { name: "Ristretto", image: "assets/images/ristretto.jpg" }
+];
+//console.log(coffees);
+
+
+const showCoffees = () => {
+  let output = "";
+  coffees.forEach(
+    ({ name, image }) =>
+      (output += `
+            <div class="card>
+            <img class="card-avatar" src=${image}/>
+            <h1 class="card-title">${name}</h1>
+            <a class="card-link" href="#">Taste<a/>
+            </div>
+        `)
+  );
+  //console.log(showCoffees);
+  
+  container.innerHTML = output;
+};
+
+//document.addEventListener("DOMContentLoaded", showCoffees);
